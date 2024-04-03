1640bcad1d448dbb1518f16758c6e13d0378a60e
Freddie Ansley <114400702+freddieansley@users.noreply.github.com>
2024-04-03 17:17:23 +0900

    Update poker.js

--- poker.js
+++ poker.js
@@ -1,3 +1,4 @@
+// this portion from line 2 - 54 will create a shuffled deck of 52 cards named uniqueDeck. 
 function cardNumber() {
     number = Math.ceil(Math.random() * 13);
       if (number === 1) {
@@ -42,7 +43,7 @@ function cardNumber() {
   };
 
   card = () => {
-  return (cardNumber() + " of " + cardSuit()); 
+  return (`${cardNumber()} of ${cardSuit()}`); 
     };
 
 const deck = []; 
@@ -51,11 +52,29 @@ const deck = [];
       deck[i] = card();
 };
 const uniqueDeck = [...new Set(deck)];
+// from 56 - 62 below I'll gather player information and assign them to seats at a 9 man poker table //  I will revisit this later and set it up for input features if I decide to turn i tinto an actual game later.
 
-   //cards were delt as the top card in deck or deck[0] being the top and alocated to each player in the order they would have been dealt in a game of holdem. more to add later as more players are seated and the dealer changes position. 
-  console.log(`Your hand is: ${deck[0]} & ${deck[2]}`);
-  console.log(`The dealer's hand is: ${deck[1]} & ${deck[3]}`);
+const newPlayer = ["Freddie", "Steve", "Bill Jr.", "Bill", "Gregg", "James", "Sharon", "Bob", "Dana", "Gerald"];
+// 60-63 should pull a up to 9 names from the list of new players starting with the oldest 9 players in the list.  the extra players will be on a wait list so to speak. 
+const player = [];
+  for (i = 0; i < 9; i++) {
+    player[i] = newPlayer[i];
+  }; 
+   //cards were delt as the top card in deck or deck[0] being the top and alocated to each player in the order they would have been dealt in a game of holdem. 
+  console.log(`${player[0]} has ${uniqueDeck[0]} & ${uniqueDeck[10]}`);
+  console.log(`${player[1]} has ${uniqueDeck[2]} & ${uniqueDeck[11]}`);
+  console.log(`${player[2]} has ${uniqueDeck[3]} & ${uniqueDeck[12]}`);
+  console.log(`${player[3]} has ${uniqueDeck[4]} & ${uniqueDeck[13]}`);
+  console.log(`${player[4]} has ${uniqueDeck[5]} & ${uniqueDeck[14]}`);
+  console.log(`${player[5]} has ${uniqueDeck[6]} & ${uniqueDeck[15]}`);
+  console.log(`${player[6]} has ${uniqueDeck[7]} & ${uniqueDeck[16]}`);
+  console.log(`${player[7]} has ${uniqueDeck[8]} & ${uniqueDeck[17]}`);
+  console.log(`${player[8]} has ${uniqueDeck[9]} & ${uniqueDeck[18]}`);
+
   
-  console.log(`The board is showing ${deck[5]}, ${deck[6]}, ${deck[7]}, ${deck[9]} & ${deck[11]}`);
-//added this to track the accuracy but I rather like it so I can see if the dealer is cheating for now. 
-  console.log(deck);
+  console.log(`The board is showing ${uniqueDeck[20]}, ${uniqueDeck[21]}, ${uniqueDeck[22]}, ${uniqueDeck[24]} & ${uniqueDeck[26]}`);
+
+  console.log(uniqueDeck);
+
+  console.log(player);
+ 
